485b6dfd72630a6216edfbfe1e348ce1c1c79cc2
John Harrington <harrington.john@husky.neu.edu>
2020-05-27 12:59:52 -0400

    adding file_getter

--- database_filler/bills/get_files_to_parse.py
+++ database_filler/bills/get_files_to_parse.py
@@ -1,8 +1,29 @@
+#!/usr/bin/env python3
 #create nested structure to comb through all bills and gather required information. 
-
-
+import os
 
 congress_data_dir = '../../../congress/data/'
 
 
-#get top level
+#get all congress bills
+def get_all_paths():
+    total_files = []
+
+    congresses = [f.path + '/bills/' for f in os.scandir(congress_data_dir) if f.is_dir()]
+    bill_types = []
+    for congress in congresses:
+        bill_types = [f.path for f in os.scandir(congress) if f.is_dir()]
+        for bill_type in bill_types:
+            bill_nums = [f.path for f in os.scandir(bill_type) if f.is_dir()]
+            for bill_num in bill_nums:
+                print(bill_num)
+                bill_states = [f.path for f in os.scandir(bill_num) if f.is_dir()]
+                for bill_state in bill_states:
+                    print(bill_states)
+
+    return total_files
+
+
+if __name__ == "__main__":
+    files = get_all_paths()
+    print(files)
--- database_filler/legislators/yaml_to_csv.py
+++ database_filler/legislators/yaml_to_csv.py
@@ -1,6 +1,6 @@
-#AIM: to get all relevant information on all legislators into database
+# AIM: to get all relevant information on all legislators into database
 
-#parse through yamls
+# parse through yamls
 import yaml
 import os
 from datetime import date
@@ -21,38 +21,34 @@ class politician(Base):
         return "<politician(name="
 """
 
-
-
-
-engine = create_engine('sqlite:///politician_term.db', echo = True)
+engine = create_engine('sqlite:///politician_term.db', echo=True)
 meta = MetaData()
 
 politician_term = Table(
-   'politician_term', meta, 
-   Column('id', Integer, primary_key = True),
-   Column('polid', String), 
-   Column('firstname', String), 
-   Column('lastname', String), 
-   Column('start_d', Date),
-   Column('end_d', Date),
-   Column('party_affiliation', String),
-   Column('state', String),
-   Column('legislative_body', String),
-   Column('gender', String),
-   Column('district', String),
-   sqlite_autoincrement=True
+    'politician_term', meta,
+    Column('id', Integer, primary_key=True),
+    Column('polid', String),
+    Column('firstname', String),
+    Column('lastname', String),
+    Column('start_d', Date),
+    Column('end_d', Date),
+    Column('party_affiliation', String),
+    Column('state', String),
+    Column('legislative_body', String),
+    Column('gender', String),
+    Column('district', String),
+    sqlite_autoincrement=True
 )
 
 
 politician = Table(
     'politician', meta,
-    Column('id', Integer, primary_key = True),
+    Column('id', Integer, primary_key=True),
     Column('bioid', String),
     Column('date_of_birth', Date),
-    #Column('race', String),
+    # Column('race', String),
     sqlite_autoincrement=True
 )
-
 
 
 meta.create_all(engine)
@@ -65,20 +61,19 @@ sess.configure(bind=engine)
 session = sess()
 
 
-
 current_yaml = 'congress-legislators/legislators-current.yaml'
 historical_yaml = 'congress-legislators/legislators-historical.yaml'
 congress_dir = '../../../congress/'
 dirname = os.path.dirname(__file__)
 filename = os.path.join(dirname, congress_dir + current_yaml)
 skips = 0
-#open up the yaml files
+# open up the yaml files
 for y in [current_yaml, historical_yaml]:
     with open(congress_dir + y) as file:
         info = yaml.full_load(file)
         for item in info:
             gender = item['bio']['gender']
-            dob = item['bio'].get('birthday','0001-01-01')
+            dob = item['bio'].get('birthday', '0001-01-01')
             dob = date(*[int(i) for i in dob.split('-')])
             if dob.year < 1900:
                 skips += 1
@@ -86,30 +81,30 @@ for y in [current_yaml, historical_yaml]:
             bio_id = item['id']['bioguide']
             firstname = item['name']['first']
             lastname = item['name']['last']
-            pol = politician.insert().values(date_of_birth = dob, bioid = bio_id)
+            pol = politician.insert().values(date_of_birth=dob, bioid=bio_id)
             result = session.execute(pol)
             for term in item['terms']:
                 start_date = date(*[int(i) for i in term['start'].split('-')])
                 end_date = date(*[int(i) for i in term['end'].split('-')])
                 if end_date.year < 1950:
                     continue
-                party = term.get('party',"NA")
+                party = term.get('party', "NA")
                 body = term['type']
                 state = term['state']
                 district = None
                 if body == 'rep':
                     district = term['district']
-                ins = politician_term.insert().values(polid = bio_id, firstname = firstname, lastname = lastname, \
-                party_affiliation= party, state = state, district = district, start_d = start_date, end_d = end_date, \
-                legislative_body = body, gender = gender)
+                ins = politician_term.insert().values(polid=bio_id, firstname=firstname, lastname=lastname,
+                                                      party_affiliation=party, state=state, district=district, start_d=start_date, end_d=end_date,
+                                                      legislative_body=body, gender=gender)
                 result = session.execute(ins)
-                
+
 
 print(skips)
 meta.bind = engine
 
 result = session.execute(politician_term.select())
-#result = conn.execute(s)
+# result = conn.execute(s)
 if os.path.exists("pol_term.csv"):
     os.remove("pol_term.csv")
 fh = open('pol_term.csv', 'wb')
