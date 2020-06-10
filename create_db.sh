e2486bf35274d2417d7375ff61bd8f245fdf4c55
John Harrington <harrington.john@husky.neu.edu>
2020-06-10 13:56:46 -0400

    Fix creation script

--- create_db.sh
+++ create_db.sh
@@ -1,9 +1,16 @@
 rm political_db.db
+echo "Removed old db"
 cd database_filler
 ./parse_legislators.py
+echo "Added legislators"
 ./parse_bills.py
+echo "Added bills"
 ./parse_sponsor.py
+echo "Added Sponsors"
 ./parse_bill_ref.py
+echo "Added Bill Refs"
 ./parse_topics.py
+echo "Added Topics"
 #./parse_votes.py
+#echo "Added votes"
 cd .. 
